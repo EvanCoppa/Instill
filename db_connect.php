a1eabfd191150928153d94d76d8d6d1e71e72edb
Evan Coppa <evancoppa@gmail.com>
2022-11-10 14:13:26 -0500

    added the database connection

---
+++ db_connect.php
@@ -0,0 +1,7 @@
+<?php
+$mysqli = mysqli_connect("localhost", "egc5187", "Literacy8+cowherd", "egc5187");
+if (mysqli_connect_errno()) {
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+	}
+?>
