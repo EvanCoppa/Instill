82a68d05f6791c43dc29cafebbfdc261034939c4
Vivian Hafener <79525734+viv-codes@users.noreply.github.com>
2022-11-19 13:49:41 -0500

    Delete db_connect.php

--- db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = mysqli_connect("localhost", "egc5187", "Literacy8+cowherd", "egc5187");
-if (mysqli_connect_errno()) {
-	printf("Connect failed: %s\n", mysqli_connect_error());
-	exit();
-	}
-?>
