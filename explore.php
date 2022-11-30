34117dacb72f7392e473402fbbd9424722ad1fa0
Vivian Hafener (RIT Student) <meh7118@ist-solace.main.ad.rit.edu>
2022-11-30 10:46:11 -0500

    Improved display of modular elements

--- explore.php
+++ explore.php
@@ -23,7 +23,7 @@
         <?php
         $result = $mysqli->query("SELECT * FROM lesson");
             while ($row = $result->fetch_assoc()) {
-                $components->createPageComponent('medium', $row['title'], '', $row['path_url']);
+                $components->createPageComponent('small', $row['title'], '', $row['path_url']);
             }
         ?>
 
